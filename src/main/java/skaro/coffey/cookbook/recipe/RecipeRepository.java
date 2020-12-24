26f6cdba2c1b274d9c495ee0f18b46b6c50197a9
unknown <bencbchurchill@gmail.com>
2020-12-24 10:58:13 -0700

    Added query param consuming logic to look at lists and consider those as 'and' predicates

--- src/main/java/skaro/coffey/cookbook/recipe/RecipeRepository.java
+++ src/main/java/skaro/coffey/cookbook/recipe/RecipeRepository.java
@@ -3,6 +3,7 @@ package skaro.coffey.cookbook.recipe;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.MultiValueBinding;
@@ -12,6 +13,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.NumberPath;
 import com.querydsl.core.types.dsl.StringPath;
 
@@ -20,7 +22,13 @@ import com.querydsl.core.types.dsl.StringPath;
 public interface RecipeRepository extends PagingAndSortingRepository<Recipe, String>, QuerydslPredicateExecutor<Recipe>, QuerydslBinderCustomizer<QRecipe> {
 	@Override
 	default public void customize(QuerydslBindings bindings, QRecipe root) {
-		bindings.bind(String.class).first((StringPath path, String value) -> path.containsIgnoreCase(value));
+		bindings.bind(String.class).first((StringPath path, String value) -> {
+			BooleanBuilder containsAllItemsInListPredicate = new BooleanBuilder();
+			Stream.of(value.split(","))
+				.forEach(item -> containsAllItemsInListPredicate.and(path.containsIgnoreCase(item)));
+			
+			return containsAllItemsInListPredicate;
+		});
 		
 		bindings.bind(root.serveCount).all((MultiValueBinding<NumberPath<Integer>, Integer>) (path, values) -> {
 			List<Integer> serveCountValues = new ArrayList<Integer>(values);
